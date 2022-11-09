fd1d8a2aec78bcb48881bc25d177afc01005e18d
syss220211 <syss220211@gmail.com>
2022-11-09 22:15:05 +0900

    button study

--- UIStudy/ContentView.swift
+++ UIStudy/ContentView.swift
@@ -10,12 +10,44 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button {
+                print("pressed") // button이 눌렸을 때 하는 일
+            } label: {
+                Image(systemName: "heart") // button 이 어떻게 생겼는지
+                    .padding(8)
+                    .tint(.red)
+                    .background(.black)
+                    .cornerRadius(13)
+                Text("SY")
+                    .padding(8)
+                    .background(.black)
+                    .tint(.orange)
+                    .cornerRadius(13)
+            }
+            
+            Button(role: .cancel) {
+                print("cancel!")
+            } label: {
+                Text("cancel!")
+                    .tint(.red)
+            }
+            
+            // naver로 이동
+            Link("NAVER", destination: URL(string: "https://www.naver.com/")!)
+                .tint(.green)
+                .frame(width: 100, height: 40)
+                .fontWeight(.bold)
+                .background(.yellow)
+            
+            Menu("Choice") {
+                Button("first"){
+                    print("press first")
+                }
+                Button("Second"){
+                    print("press second")
+                }
+            }
         }
-        .padding()
     }
 }
 
---
+++ UIStudy/TestView.swift
@@ -0,0 +1,20 @@
+//
+//  TestView.swift
+//  UIStudy
+//
+//  Created by 박서연 on 2022/11/09.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
