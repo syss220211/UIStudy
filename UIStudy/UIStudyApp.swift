7cd83855132839dca13da74d337873c5f284cbe8
syss220211 <syss220211@gmail.com>
2022-11-09 21:28:05 +0900

    Initial Commit

---
+++ UIStudy/UIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIStudyApp.swift
+//  UIStudy
+//
+//  Created by 박서연 on 2022/11/09.
+//
+
+import SwiftUI
+
+@main
+struct UIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
