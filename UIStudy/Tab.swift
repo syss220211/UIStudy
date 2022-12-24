3aa9fc390f44e02c63952487687ff7e3895e9c7c
syss220211 <syss220211@gmail.com>
2022-12-25 01:46:49 +0900

    swiftUI TabView

--- UIStudy/Tab.swift
+++ UIStudy/Tab.swift
@@ -8,9 +8,13 @@
 import SwiftUI
 
 struct Tab: View {
+    
+    @State var isShowingModal: Bool = true
+    
     var body: some View {
+        
         TabView {
-            ReceivedView()
+            ReceivedView(modalState: $isShowingModal)
                 .badge(2)
                 .tabItem {
                     Label("Received", systemImage: "tray.and.arrow.down.fill")
@@ -24,13 +28,28 @@ struct Tab: View {
                 .tabItem {
                     Label("Account", systemImage: "person.crop.circle.fill")
                 }
-        }
+        }.accentColor(.yellow) // 선택시 바뀌는 color 변경
     }
 }
 
 struct ReceivedView: View {
+    
+    @Binding var modalState: Bool
+    
     var body: some View {
         Text("ReceivedView")
+        Button {
+            modalState = false
+            
+        } label: {
+            Text("닫기")
+                .frame(width: 200, height: 30)
+        }
+//        VStack {
+//            Text("품종선택.. ?")
+//
+//
+//        }
     }
 }
 
